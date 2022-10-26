a9a7bf9c82be42092e36e60ff7aa1c54978b8dac
Carolina Cartategui <carolina_cartategui@hotmail.com>
2022-10-25 23:00:19 -0300

    modificaciones en models

--- blog/models.py
+++ blog/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 class Configuracion(models.Model):
     nombre_blog = models.CharField(max_length=14)
+    contruido_por = models.CharField(max_length=30)
+    titulo_principal = models.CharField(max_length=30, default="")
+    subtitulo_principal = models.CharField(max_length=60, default="")
+
